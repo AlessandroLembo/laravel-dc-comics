d49109992ac378c0b1904ab7fbc97823c811920b
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-02 19:40:25 +0100

    changes header scss

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,11 @@ Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 // Rotta per modificare i dati di un DC comic
 Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
 
-// Rotta per elminare un DC comic
-Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
-
 // Rotta per l'update di un DC comic
 Route::put('/comics/{comic}', [ComicController::class, 'update'])->name('comics.update');
+
+// Rotta per elminare un DC comic
+Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
 
 // Rotta per visualizzare i dettagli del singolo dc comic
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
